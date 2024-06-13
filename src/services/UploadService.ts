2e4e07652e0ce66b5816a623b14c2cd85f0b5930
Guilherme-Matosoli <guilhermematosgto2015@outlook.com>
2024-06-13 14:17:36 -0300

    create UploadService :rocket:

---
+++ src/services/UploadService.ts
@@ -0,0 +1,30 @@
+import path from "path";
+import fs from "fs";
+import { MultipartFile } from "@fastify/multipart";
+
+interface UploadProps {
+  file: MultipartFile,
+  token: string
+};
+
+export class UploadServide {
+  async upload({ file, token }: UploadProps) {
+    try {
+      const chunks: Buffer[] = [];
+      for await (const chunk of chunks) {
+        chunks.push(chunk)
+      };
+
+      const buffer = Buffer.concat(chunks);
+      const uploadDir = path.resolve(__dirname, "../../public/uploads");
+      const filePath = path.join(uploadDir, file.filename)
+
+      fs.writeFileSync(filePath, buffer);
+
+      return "Upload succesfully";
+    }
+    catch (err) {
+      console.log(err)
+    };
+  };
+}
