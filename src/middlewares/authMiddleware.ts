0d0c0bd8ad37f922f141611cb62852cc051cddbd
Guilherme-Matosoli <guilhermematosgto2015@outlook.com>
2024-06-13 19:34:55 -0300

    return 401 in middleware if token is invalid :rocket:

--- src/middlewares/authMiddleware.ts
+++ src/middlewares/authMiddleware.ts
@@ -3,11 +3,18 @@ import { DecryptTokenService } from "../services/DecryptTokenService";
 
 
 export const autMiddleware = (req: FastifyRequest, reply: FastifyReply, done: HookHandlerDoneFunction) => {
-  const token = req.headers.authorization as string;
-  if (!token) return reply.status(401).send({ message: "Missing authorization token" });
+  try {
 
-  const userId = new DecryptTokenService().decrypt(token);
-  req.headers.cookie = userId;
 
-  done();
+    const token = req.headers.authorization as string;
+    if (!token) return reply.status(401).send({ message: "Missing authorization token" });
+
+    const userId = new DecryptTokenService().decrypt(token);
+    req.headers.cookie = userId;
+
+    done();
+  }
+  catch (err) {
+    return reply.status(401).send({ message: "Invalid token" });
+  }
 };
