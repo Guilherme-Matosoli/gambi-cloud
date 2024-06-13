db395bc89c0a528c628c0ad9309c1d76eef40c6c
Guilherme-Matosoli <guilhermematosgto2015@outlook.com>
2024-06-13 15:03:39 -0300

    add a route to create token :tada:

--- src/server.ts
+++ src/server.ts
@@ -3,12 +3,15 @@ import fastify from "fastify";
 import multipart from "@fastify/multipart";
 
 import { UploadController } from "./controllers/UploadController";
+import { CreateTokenController } from './controllers/CreateTokenController';
 
 export const app = fastify();
 app.register(multipart);
 
 
 app.post('/upload', new UploadController().upload);
+
+app.post('/token/create', new CreateTokenController().create);
 
 app.listen({ port: 4000 }).then(() => {
   console.log("Server is running on port 4000")
