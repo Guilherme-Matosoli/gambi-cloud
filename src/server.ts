90c06acb206246d328fd78a9c8f45827169b2aa9
Guilherme-Matosoli <guilhermematosgto2015@outlook.com>
2024-06-13 12:28:32 -0300

    configure fastify multipart and create upload route :fire:

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,13 @@
 import fastify from "fastify";
+import multipart from "@fastify/multipart";
 
-const app = fastify();
+import { UploadController } from "./controllers/UploadController";
+
+export const app = fastify();
+app.register(multipart);
+
+
+app.post('/upload', new UploadController().upload);
 
 app.listen({ port: 4000 }).then(() => {
   console.log("Server is running on port 4000")
