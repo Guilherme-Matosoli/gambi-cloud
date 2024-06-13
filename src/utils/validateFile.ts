fd8cc6628882859a0c1f1107819602444c401fb1
Guilherme-Matosoli <guilhermematosgto2015@outlook.com>
2024-06-13 17:06:59 -0300

    add function to validate file :tada:

---
+++ src/utils/validateFile.ts
@@ -0,0 +1,28 @@
+import { MultipartFile } from "@fastify/multipart";
+
+interface ValidateFileProps {
+  file: MultipartFile,
+  fileSize: number
+};
+
+
+export const validateFile = ({ file, fileSize }: ValidateFileProps) => {
+  const maxSize = 5 * 1024 * 1024;
+
+  const acceptedMimeTypes = [
+    "image/jpeg",
+    "image/jpg",
+    "image/png",
+    "image/gif",
+    "image/bmp",
+    "image/webp",
+    "image/tiff",
+    "image/svg+xml"
+  ];
+
+  if (!acceptedMimeTypes.includes(file.mimetype)) return "File extension not accept";
+
+  if (fileSize > maxSize) return "Max size exceeded limit (5MB)";
+
+  return null;
+};
