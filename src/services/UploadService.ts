60e0d0fec8ed0e7e7356bf4522068ce1b2ebbeb5
Guilherme-Matosoli <guilhermematosgto2015@outlook.com>
2024-06-13 17:09:02 -0300

    add file validation on upload service :rocket:

--- src/services/UploadService.ts
+++ src/services/UploadService.ts
@@ -1,6 +1,7 @@
 import path from "path";
 import fs from "fs";
 import { MultipartFile } from "@fastify/multipart";
+import { validateFile } from "../utils/validateFile";
 
 interface UploadProps {
   file: MultipartFile,
@@ -11,13 +12,18 @@ export class UploadServide {
   async upload({ file, token }: UploadProps) {
     try {
       const chunks: Buffer[] = [];
+      let fileSize = 0;
       for await (const chunk of file.file) {
+        fileSize += chunk.length;
         chunks.push(chunk)
       };
 
+      const fileValidation = validateFile({ file, fileSize });
+      if (!fileValidation != null) return fileValidation;
+
       const buffer = Buffer.concat(chunks);
       const uploadDir = path.resolve(__dirname, "../../public/uploads");
-      const filePath = path.join(uploadDir, file.filename)
+      const filePath = path.join(uploadDir, file.filename);
 
       fs.writeFileSync(filePath, buffer);
 
