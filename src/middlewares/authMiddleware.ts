93d0dc50c6d7917b4b97b1cd1f4148b4d71c1dc4
Guilherme-Matosoli <guilhermematosgto2015@outlook.com>
2024-06-13 19:31:03 -0300

    add an auth middleware :fire:

---
+++ src/middlewares/authMiddleware.ts
@@ -0,0 +1,13 @@
+import { FastifyReply, FastifyRequest, HookHandlerDoneFunction } from "fastify";
+import { DecryptTokenService } from "../services/DecryptTokenService";
+
+
+export const autMiddleware = (req: FastifyRequest, reply: FastifyReply, done: HookHandlerDoneFunction) => {
+  const token = req.headers.authorization as string;
+  if (!token) return reply.status(401).send({ message: "Missing authorization token" });
+
+  const userId = new DecryptTokenService().decrypt(token);
+  req.headers.cookie = userId;
+
+  done();
+};
