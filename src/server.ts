6bfd73a83dc174561a090fc317ba3b9d9b5a7be8
Guilherme-Matosoli <guilhermematosgto2015@outlook.com>
2024-06-13 18:36:09 -0300

    add a route to get images :fire:

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,7 @@ import multipart from "@fastify/multipart";
 
 import { UploadController } from "./controllers/UploadController";
 import { CreateTokenController } from './controllers/CreateTokenController';
+import { GetImagesController } from './controllers/GetImagesController';
 
 export const app = fastify();
 app.register(multipart);
@@ -12,6 +13,8 @@ app.register(multipart);
 app.post('/upload', new UploadController().upload);
 
 app.post('/token/create', new CreateTokenController().create);
+
+app.get('/images/:userId', new GetImagesController().get);
 
 app.listen({ port: 4000 }).then(() => {
   console.log("Server is running on port 4000")
