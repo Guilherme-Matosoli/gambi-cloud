efa2a378812bbfc53497f6b7755e37a7d579ed1c
Guilherme-Matosoli <guilhermematosgto2015@outlook.com>
2024-06-14 13:22:45 -0300

    solve file exists verification on render service :bug:

--- src/services/RenderImageService.ts
+++ src/services/RenderImageService.ts
@@ -11,7 +11,7 @@ export class RenderImageService {
   async render({ token, filename }: RenderImageProps) {
     try {
       const filePath = path.resolve(__dirname, "../../public/uploads/", token, filename);
-      const fileExists = verifyFileExists(filePath);
+      const fileExists = await verifyFileExists(filePath);
       if (!fileExists) return "Image does not exists";
 
       const stream = fs.createReadStream(filePath);
